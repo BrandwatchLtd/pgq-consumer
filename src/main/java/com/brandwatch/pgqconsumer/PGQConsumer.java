6dbe6ab6087c5fa46881c34f18141a3f211bbd54
James Stanier <jamess@brandwatch.com>
2015-02-05 16:40:54 +0000

    Remove unused imports

--- src/main/java/com/brandwatch/pgqconsumer/PGQConsumer.java
+++ src/main/java/com/brandwatch/pgqconsumer/PGQConsumer.java
@@ -6,8 +6,6 @@ import javax.sql.DataSource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Required;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
